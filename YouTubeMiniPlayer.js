bfc1674e8b1215aa070b0438cbe65ad1256df2b0
AkiyaKiko <sinchialveno@protonmail.com>
2025-04-22 18:05:52 +0800

    change version

--- YouTubeMiniPlayer.js
+++ YouTubeMiniPlayer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Mini Player
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      2.1
 // @license      MIT
 // @description  Youtube Mini Player. When you scroll down the mini player will appear.
 // @author       https://github.com/AkiyaKiko
